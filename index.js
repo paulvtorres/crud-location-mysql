5e169528627a146e1f60d60d5de79ac9d37ac19f
paulvtorres <87663669+paulvtorres@users.noreply.github.com>
2024-05-30 15:56:00 -0500

    arreglos cierres de bd

--- index.js
+++ index.js
@@ -14,38 +14,54 @@ const app = express();
 const PORT = 5000;
 app.use(bodyParser.json())
 
-initcon()
+
+app.listen(PORT, () => {
+    // console.log("SERVER: http://localhost:${PORT}");
+    initcon()  
+})
+
+
+
 function initcon(){
-app.listen(PORT,()=> {
-    console.log("SERVER: http://localhost:${PORT}");
-    connection.connect((err) =>{
-        if(err) {
+    console.log("SERVER: http://localhost: " + PORT);
+    connection.connect((err) => {
+        if (err) {
             console.log("DATABASE UNCONNECTED")
-            throw err};
+        }
+        else
         console.log("DATABASE CONNECTED")
     })
-})
 }
 
 app.use("/all",(req,res) =>{
     try {
     const sql_query = "select * from location";
+        console.log("ADENTRO: " + connection)
     connection.query(sql_query,(err,result) =>{
         if (err) {
-            console.log("DATABASE UNCONNECTED")
-            if (connection == null)
-                initcon()
-           // throw err
-        };
+            
+            console.log("Error All: "+err)
+            const responseData = {
+                success: false,
+                data: null,
+                message: err.message
+            }
+            res.status(500).json(responseData);
+           initcon()
+        }
+        else{
         const responseData = {
             success: true,
             data: result,
             message: "successfull"
             }
         res.send(responseData);
+        console.log("Consulta OK")
+        }
     })
     }
     catch (err) {
+        console.log("Error: " + err)
         const responseData = {
             success: false,
             data: null,
@@ -60,9 +76,7 @@ app.use("/all",(req,res) =>{
         const phoneNum = req.params.phoneNum;
         connection.query('SELECT * FROM location WHERE phoneNum = ?',[phoneNum], (err, result) => {
             if (err) {
-                console.log("DATABASE UNCONNECTED")
-                if (connection = null)
-                    initcon()
+                console.log("Error: List" + err)
                 // throw err
             };
             const responseData = {
@@ -74,6 +88,7 @@ app.use("/all",(req,res) =>{
         })
     }
     catch (err) {
+        console.log("Error: " + err)
         const responseData = {
             success: false,
             data: null,
@@ -87,13 +102,11 @@ app.post('/',(req, res) => {
     var phoneNum = req.body.phoneNum;
     var lat = req.body.lat;
     var lng = req.body.lng;
-    connection.query('INSERT INTO location (phoneNum,name,lastName,lat,lng,lastDate) VALUES ("' + phoneNum + '","","",' + lat + ',' + lng + ',null) ON DUPLICATE KEY UPDATE lat = ' + lat + ', lng = ' + lng + ', lastDate = date_add(now(), interval - 5 hour)', (err, results) => {
+    var query = 'INSERT INTO location (phoneNum,name,lastName,lat,lng,lastDate) VALUES ("' + phoneNum + '","","",' + lat + ',' + lng + ',null) ON DUPLICATE KEY UPDATE lat = ' + lat + ', lng = ' + lng + ', lastDate = date_add(now(), interval - 5 hour)'
+    console.log(query)
+    connection.query(query, (err, results) => {
         if (err) {
-            console.error(err);
-            
-                console.log("DATABASE UNCONNECTED")
-                if (connection == null)
-                    initcon()
+            console.log("Error1: Insert" + err)
                 // throw err
             
             const responseData = {
@@ -102,6 +115,7 @@ app.post('/',(req, res) => {
                 message: err.message
             }
             res.status(500).send(responseData);
+            initcon()
         } else {
             const responseData = {
                 success: true,
